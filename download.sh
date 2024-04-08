6ffdd0c2f8f410d261944964672ec4b45961a192
cquest <cquest@openstreetmap.fr>
2024-04-08 19:46:44 +0200

    ajout script téléchargement des données

---
+++ download.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# script de téléchargement des données IM2 de Météo-France deuis data.gouv.fr
+
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_1958-1959.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_1960-1969.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_1970-1979.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_1980-1989.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_1990-1999.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_2000-2009.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_2010-2019.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/QUOT_SIM2_2020-202402.csv.gz
+wget https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/REF_CC/SIM/coordonnees_grille_safran_lambert-2-etendu.csv
+
